0c609b0753fbe8575b3da39999c069f29da08409
Michael Adam <obnox@samba.org>
2025-11-25 19:14:51 +0100

    initialize gontract
    Signed-off-by: Michael Adam <obnox@samba.org>

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/obnoxxx/gontract
+
+go 1.25.1
---
+++ gontract.go
@@ -0,0 +1,34 @@
+package gontract
+
+// The ipurpose of the gontract package is to provide assert-style condition functions
+// in order to allow implementing pre- and postconditions for functions in
+// the spirit of "design by contract".
+
+import "fmt"
+
+type Kind int
+
+const (
+	KindPre  Kind = iota // 0
+	KindPost             // 1
+)
+
+func (k Kind) String() string {
+	switch k {
+	case KindPre:
+		return "precondition"
+	case KindPost:
+		return "postcondition"
+	default:
+		return "invalid kind"
+
+	}
+}
+
+func Condition(predicate bool, kind Kind, msg string) {
+	if !predicate {
+		message := fmt.Sprintf("%s (%v) not satisfied - software bug!?", kind, msg)
+		panic(message)
+
+	}
+}
